e9ae7f8862950bd48e1f2c64828d8403ff627186
adamAllAround <adam@allaroundjava.com>
2020-07-24 15:56:05 +0200

    Moving unnecessary objects out of the domain

--- src/main/java/com/allaroundjava/cardops/domain/ports/WithdrawalMessageSender.java
+++
@@ -1,7 +0,0 @@
-package com.allaroundjava.cardops.domain.ports;
-
-import com.allaroundjava.cardops.domain.model.Withdrawal;
-
-public interface WithdrawalMessageSender {
-    void sendMessageForNew(Withdrawal withdrawal);
-}
