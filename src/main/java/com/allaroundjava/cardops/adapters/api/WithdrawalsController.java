fad16f68e0c4c4945dbe63836c00b5ace319f5ac
adamAllaround <adam@allaroundjava.com>
2020-10-10 20:48:16 +0200

    Re-configuring api paths and addidng log4j2 logging

--- src/main/java/com/allaroundjava/cardops/adapters/api/WithdrawalsController.java
+++ src/main/java/com/allaroundjava/cardops/adapters/api/WithdrawalsController.java
@@ -6,6 +6,7 @@ import com.allaroundjava.cardops.domain.ports.WithdrawingService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/withdrawals")
+@RequestMapping("/creditcards")
 @RequiredArgsConstructor
+@Log4j2
 class WithdrawalsController {
     private final WithdrawingService withdrawingService;
 
-    @PostMapping("/{cardNumber}")
+    @PostMapping("/{cardNumber}/withdrawals")
     ResponseEntity<WithdrawalResponse> withdraw(@PathVariable String cardNumber, @RequestBody WithdrawalRequest request) {
+        log.debug(String.format("Received request to withdraw %.2f from card %s", request.amount, cardNumber));
         Result result = withdrawingService.withdraw(new WithdrawCommand(cardNumber, request.getAmount()));
         if (result == Result.FAILURE) {
+            log.debug(String.format("Withdrawal attempt failed for card %s", cardNumber));
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
 
